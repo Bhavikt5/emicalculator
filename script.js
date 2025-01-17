f0f9e5e49d2916c1d7ad51b808a0ed18640345e3
Bhavik Tank <bhaviktank5@gmail.com>
2025-01-17 19:07:52 +0530

    style updated

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 window.onload = function () {
+  setLoanAmountFromURL();
   calculateEMI();
 };
+
+function setLoanAmountFromURL() {
+  const url = window.location.href;
+  const match = url.match(/\/(\d+)-personal-loan\//);
+
+  // Default value if no match is found
+  const defaultLoanAmount = match ? parseInt(match[1]) : 3000;
+
+  // Validate loan amount is within range
+  const minLoanAmount = 3000;
+  const maxLoanAmount = 500000;
+  const loanAmount = Math.max(
+    minLoanAmount,
+    Math.min(defaultLoanAmount, maxLoanAmount)
+  );
+
+  // Set values for input and slider
+  const loanAmountInput = document.getElementById("loanAmount");
+  const loanAmountBar = document.getElementById("loanAmountBar");
+  loanAmountInput.value = loanAmount;
+  loanAmountBar.value = loanAmount;
+}
+
 function updateLoanAmount() {
   const loanAmountBar = document.getElementById("loanAmountBar");
   const loanAmount = document.getElementById("loanAmount");
   loanAmount.value = loanAmountBar.value;
+}
+function updateLoanAmountBar() {
+  const loanAmountBar = document.getElementById("loanAmountBar");
+  const loanAmount = document.getElementById("loanAmount");
+  loanAmountBar.value = loanAmount.value;
 }
 
 function updateInterestRate() {
